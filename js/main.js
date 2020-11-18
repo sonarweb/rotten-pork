ed5b97a281f60fd5eec803baf99288fd06bf0ae7
unknown <axel.hstr@gmail.com>
2020-11-18 14:10:24 -0600

    Menu actualizado

--- js/main.js
+++ js/main.js
@@ -1,24 +1,103 @@
-const navSlide = () =>{
-    const burguer = document.querySelector('#burguer');
-    const nav = document.querySelector('#nav-ul');
-    burguer.addEventListener('click', () =>{
-    nav.classList.toggle('nav-active');
-});
+!function(e) {
+    var t= {
 }
-
-navSlide();
-
-let ubicaciónPrincipal = window.pageYOffset;
-
-window.addEventListener("scroll", function(){
-    let desplazamientoAcual = window.pageYOffset;
-    if(ubicaciónPrincipal>=desplazamientoAcual){
-        document.getElementsByTagName("nav")[0].style.top = "0px"
-    } else{
-        document.getElementsByTagName("nav")[0].style.top = "-100px"
-    }
-    ubicaciónPrincipal = desplazamientoAcual;
-})
+;
+    function o(n) {
+    if(t[n])return t[n].exports;
+    var r=t[n]= {
+    i: n, l:!1, exports: {
+}
+}
+;
+    return e[n].call(r.exports, r, r.exports, o), r.l=!0, r.exports;
+}
+o.m=e, o.c=t, o.d=function(e, t, n) {
+    o.o(e, t)||Object.defineProperty(e, t,  {
+    enumerable: !0, get:n,
+}
+);
+}, o.r=function(e) {
+    "undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e, Symbol.toStringTag,  {
+    value: "Module"}
+), Object.defineProperty(e, "__esModule",  {
+    value: !0,
+}
+);
+}, o.t=function(e, t) {
+    if(1&t&&(e=o(e)), 8&t)return e;
+    if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;
+    var n=Object.create(null);
+    if(o.r(n), Object.defineProperty(n, "default",  {
+    enumerable: !0, value:e,
+}
+), 2&t&&"string"!=typeof e)for(var r in e)o.d(n, r, function(t) {
+    return e[t];
+}
+.bind(null, r));
+    return n;
+}
+, o.n=function(e) {
+    var t=e&&e.__esModule?function() {
+    return e.default;
+}
+: function() {
+    return e;
+}
+;
+    return o.d(t, "a", t), t;
+}
+, o.o=function(e, t) {
+    return Object.prototype.hasOwnProperty.call(e, t);
+}
+, o.p="/", o(o.s=1);
+}( {
+    1: function(e, t, o) {
+    e.exports=o("8yrV");
+}
+, "8yrV": function(e, t) {
+    function o() {
+    $(window).scrollTop()>200?$("#navigation").addClass("colorized"): $("#navigation").removeClass("colorized");
+}
+function n() {
+    $("#navigation .sidebar").addClass("show"), function() {
+    if(!$("#backdrop").length) {
+    document.body.style.overflow="hidden";
+    var e=$('<div class="backdrop fade" id="backdrop"></div>');
+    $("body").append(e);
+    e.outerHeight();
+    e.addClass("show");
+}
+}();
+}function r() {
+    $("#navigation .sidebar").removeClass("show"), function() {
+    if($("#backdrop").length) {
+    document.body.style.overflow="";
+    var e=$("#backdrop");
+    e.removeClass("show");
+    var t=function(e) {
+    if(!e)return 0;
+    var t=$(e).css("transition-duration"), o=$(e).css("transition-delay"), n=parseFloat(t), r=parseFloat(o);
+    if(!n&&!r)return 0;
+    return t=t.split(", ")[0], o=o.split(", ")[0], 1e3*(parseFloat(t)+parseFloat(o));
+}
+(e);
+    setTimeout((function() {
+    e.remove();
+}
+), t);
+}}();
+}function i() {
+    window.innerWidth>=992&&r();
+}
+$((function() {
+    var e, t;
+    $("#navigation").length&&(o(), $("#show-sidebar").click(n), $("#hide-sidebar").click(r), $("body").on("click", "#backdrop", r), $(window).scroll((function() {
+    e&&window.clearTimeout(e), e=window.setTimeout(o, 100);
+}
+)), $(window).on("orientation resize", (function() {
+    clearTimeout(t), t=setTimeout(i, 500);
+}
+)));}))}})
 
 
 
