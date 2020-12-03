db2d629e8d4fa2d0de439e4600d18248f9a2cef0
unknown <axel.hstr@gmail.com>
2020-12-02 23:34:05 -0600

    cambio de nombre a clase en footer links

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,12 @@
 		<div class="footer__logo">
 			<img src="img/logo-rotten-pork-white.png" alt="">
 		</div>
-		<div class="social">
-			<ul class="social__icons">
-				<li class="social__icon"><a href="https://www.facebook.com/rottenpork"><i class="fa fa-facebook"></i></a></li>
-				<li class="social__icon"><a href="https://open.spotify.com/artist/6yAir8RriqqAEKPasw0jnn?si=oOFwI9dHRzSkTsc_UAui5Q"><i class="fab fa-spotify"></i></a></li>
-				<li class="social__icon"><a href="https://rottenpork.bandcamp.com/"><i class="fab fa-bandcamp"></i></a></li>
-				<li class="social__icon"><a href="https://www.youtube.com/user/RottenPorkOfficial"><i class="fab fa-youtube"></i></a></li>
+		<div class="footer__social">
+			<ul class="footer__icons">
+				<li class="footer__icon"><a href="https://www.facebook.com/rottenpork"><i class="fa fa-facebook"></i></a></li>
+				<li class="footer__icon"><a href="https://open.spotify.com/artist/6yAir8RriqqAEKPasw0jnn?si=oOFwI9dHRzSkTsc_UAui5Q"><i class="fab fa-spotify"></i></a></li>
+				<li class="footer__icon"><a href="https://rottenpork.bandcamp.com/"><i class="fab fa-bandcamp"></i></a></li>
+				<li class="footer__icon"><a href="https://www.youtube.com/user/RottenPorkOfficial"><i class="fab fa-youtube"></i></a></li>
 			</ul>
 		</div>
 		<div class="footer__menu">
