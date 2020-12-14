3aa225c118f2c6a3bbe717a60439d14f006ec07e
sonarweb <axel.hstr@gmail.com>
2020-12-14 14:10:56 -0600

    Favicon añadido

--- en/includes/header.php
+++ en/includes/header.php
@@ -17,6 +17,7 @@
     <meta name="language" content="English">
     <meta name="author" content="Axl Sánchez">
 	<title><?php echo $title ?></title>
+    <link rel="icon" href="img/favicon.png">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<link rel="stylesheet" href="../css/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
