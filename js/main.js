e48d50d243cc0f079cdcd974b5ef9541b7345bc2
sonarweb <axel.hstr@gmail.com>
2020-12-22 19:02:01 -0600

    Funcion para diferir carga de iframe en index

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/*Parsing Iframe*/
-function init() {
-    var vidDefer = document.getElementsByTagName('iframe');
-    for (var i=0; i<vidDefer.length; i++) {
-    if(vidDefer[i].getAttribute('data-src')) {
-    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-    } } }
-
-    window.onload = init;
-
 !function(e) {
     var t= {
 }
