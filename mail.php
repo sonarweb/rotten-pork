d8732b4fd59bef692f813ff525218210da9ae2a7
sonarweb <axel.hstr@gmail.com>
2020-12-26 13:39:30 -0600

    mail php modificado

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-    $mainemail = 'axel.hstr@gmail.com';
+    $mainemail = 'axel.sanchez@rottenporkofficial.com';
     // Correo al que va a llegar el mensaje
     $name = $_POST['nombre'];
     $city = $_POST['ciudad'];
@@ -9,7 +9,7 @@
     $header = "Enviado desde el sitio oficial de Rotten Pork";
     $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
     
-    mail($name, $city, $mail, $mensaje, $header);
+    mail($mainemail, $name, $city, $mail, $mensaje, $header);
     echo "<script>alert('Correo enviado exitosamente')</script>";
     echo "<script> setTimeout(\"location.href='contacto.php'\",1000)</script>";
 ?>
